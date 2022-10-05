55b3954beb6994d7166199fdee1e3c85c6705afc
Sanjay <sanjayjadhav465@gmail.com>
2022-10-05 16:19:01 +0530

    Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import '../styles/App.css';
+import { useState, useEffect } from 'react';
 const App = () => {
+  const[date, setDate]=  useState();
+
+  useEffect(()=>{
+    const id = setInterval(()=>{setDate(new Date().toLocaleString())}, 1000)
+    
+
+    return ()=>{
+        clearInterval(id);
+    }
+  })
 
   return (
     <div id="main">
-      <div className="date-time"></div>
+      <div className="date-time">{date}</div>
     </div>
   )
 }
